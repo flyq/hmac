08bb0f2f8f817b795f9038b403b2f68a5e64ab33
flyq <flyq951@gmail.com>
2020-04-08 07:46:33 -0700

    add to_string

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 extern crate hmac;
 extern crate sha2;
+extern crate hex;
 
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
+use hex::encode;
 
 // Create alias for HMAC-SHA256
 type HmacSha256 = Hmac<Sha256>;
@@ -34,6 +36,11 @@ fn main() {
     // incorrect use of the code value may permit timing attacks which defeat
     // the security provided by the `MacResult`
     let code_bytes = result.code();
+    let code = hex::encode(code_bytes);
+
+    assert_eq!(code.to_string(), String::from("97d2a569059bbcd8ead4444ff99071f4c01d005bcefe0d3567e1be628e5fdcd9"));
+
+    println!("{:?}", code);
     print_type_of(&code_bytes);
     println!("code_bytes: {:?}", code_bytes);
     let mut mac = HmacSha256::new_varkey(b"my secret and secure key")
@@ -60,6 +67,7 @@ hmac::Hmac<sha2::sha256::Sha256>
 key+msg: Hmac { digest: Sha256 { ... }, i_key_pad: [91, 79, 22, 69, 83, 85, 68, 83, 66, 22, 87, 88, 82, 22, 69, 83, 85, 67, 68, 83, 22, 93, 83, 79, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54], opad_digest: Sha256 { ... } }
 hmac::Hmac<sha2::sha256::Sha256>
 crypto_mac::MacResult<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UTerm, typenum::bit::B1>, typenum::bit::B0>, typenum::bit::B0>, typenum::bit::B0>, typenum::bit::B0>, typenum::bit::B0>>
+"97d2a569059bbcd8ead4444ff99071f4c01d005bcefe0d3567e1be628e5fdcd9"
 generic_array::GenericArray<u8, typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UInt<typenum::uint::UTerm, typenum::bit::B1>, typenum::bit::B0>, typenum::bit::B0>, typenum::bit::B0>, typenum::bit::B0>, typenum::bit::B0>>
 code_bytes: [151, 210, 165, 105, 5, 155, 188, 216, 234, 212, 68, 79, 249, 144, 113, 244, 192, 29, 0, 91, 206, 254, 13, 53, 103, 225, 190, 98, 142, 95, 220, 217]
 Hello, world!()
